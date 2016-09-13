4c3f01d3bebf6e86969efa12964ac4e84e0207b8
Rinat Khabibiev <srenskiy@gmail.com>
2016-09-13 10:47:59 +0300

    new version: 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as description:
 
 setup(
     name='django-cache',
-    version='0.2',
+    version='0.2.1',
     author='Rinat Khabibiev',
     author_email='srenskiy@gmail.com',
     py_modules=['djangocache'],
